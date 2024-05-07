0d7111c5bae51142e272ae3167742a78b83b9c2b
Rapilias <rapilias.rest@gmail.com>
2024-05-08 07:58:50 +0900

    Add assigne parent

--- src/NavStack/Assets/NavStack/Runtime/INavigationSheet.cs
+++ src/NavStack/Assets/NavStack/Runtime/INavigationSheet.cs
@@ -5,6 +5,7 @@ namespace NavStack
 {
     public interface INavigationSheet : INavigation
     {
+        void AssigneParent(IPage page);
         UniTask AddAsync(IPage page, CancellationToken cancellationToken = default);
         UniTask RemoveAsync(IPage page, CancellationToken cancellationToken = default);
         UniTask RemoveAllAsync(CancellationToken cancellationToken = default);
