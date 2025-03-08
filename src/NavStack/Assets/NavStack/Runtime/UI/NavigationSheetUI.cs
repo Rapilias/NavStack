1168f218ed8e2e99f939f2c49c7b6fb8e51a6784
Rapilias <rapilias.rest@gmail.com>
2025-03-08 22:15:18 +0900

    Expose transform

--- src/NavStack/Assets/NavStack/Runtime/UI/NavigationSheetUI.cs
+++ src/NavStack/Assets/NavStack/Runtime/UI/NavigationSheetUI.cs
@@ -12,7 +12,8 @@ namespace NavStack.UI
     [RequireComponent(typeof(RectTransform))]
     public class NavigationSheetUI : MonoBehaviour, INavigationSheet
     {
-        [SerializeField] RectTransform parentTransform;
+        [SerializeField] 
+        protected RectTransform parentTransform;
 
         readonly NavigationSheetCore core = new();
 
