822b2894edfd9ec10928ac05390b071885b49a5d
Rapilias <91034223+Rapilias@users.noreply.github.com>
2025-03-29 01:25:58 +0900

    Revert "Add last selection to context"

--- src/NavStack/Assets/NavStack/Runtime/NavigationContext.cs
+++ src/NavStack/Assets/NavStack/Runtime/NavigationContext.cs
@@ -1,11 +1,8 @@
-using UnityEngine;
-
 namespace NavStack
 {
     public record NavigationContext
     {
         public NavigationAwaitOperation AwaitOperation { get; init; } = NavigationAwaitOperation.Sequential;
         public NavigationParameters Parameters { get; init; } = new();
-        public GameObject LastSelection { get; init; } = null;
     }
 }
